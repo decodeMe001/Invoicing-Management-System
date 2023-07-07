03eb35db2143bbb491a666f4a61ba80599f5fb4a
decodeMe001 <codedaabiola@gmail.com>
2023-07-07 12:54:50 +0100

    fix: product deduction error

--- utility/CartItems.php
+++ utility/CartItems.php
@@ -1,15 +1,7 @@
 <?php 
 namespace utility;
 
-<<<<<<< HEAD
 if (!defined('BASEPATH')) exit('No direct script access allowed');
-=======
-<<<<<<< HEAD
-if (!defined('BASEPATH')) exit('No direct script access allowed');
-=======
-if ( ! defined('BASEPATH')) exit('No direct script access allowed');
->>>>>>> 060766fe05b38dadf2897b881fab97884399e5e3
->>>>>>> 0da628fe696508bd39c21c22e2116dbf7925a3e3
 defined('BASEPATH') OR exit('No direct script access allowed');
 
 
@@ -20,37 +12,17 @@ class CartItems
     private $price;
     private $nairaSign;
 
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
->>>>>>> 0da628fe696508bd39c21c22e2116dbf7925a3e3
     public function __construct($name = '', $price = '', $nairaSign = true)
     {
         $this->name = $name;
         $this->price = $price;
         $this->nairaSign = $nairaSign;
-<<<<<<< HEAD
-=======
-=======
-    public function __construct($name = '', $price = '', $nairaSign = false)
-    {
-        $this -> name = $name;
-        $this -> price = $price;
-        $this -> nairaSign = $nairaSign;
->>>>>>> 060766fe05b38dadf2897b881fab97884399e5e3
->>>>>>> 0da628fe696508bd39c21c22e2116dbf7925a3e3
     }
 
     public function __toString()
     {
-<<<<<<< HEAD
         $rightCols = 6;
         $leftCols = 30;
-=======
-        $rightCols = 10;
-        $leftCols = 38;
-<<<<<<< HEAD
->>>>>>> 0da628fe696508bd39c21c22e2116dbf7925a3e3
         if ($this->nairaSign) {
             $leftCols = $leftCols / 2 - $rightCols / 2;
         }
@@ -58,18 +30,6 @@ class CartItems
         $nairaUnicode = "N";
         $sign = ($this->nairaSign ? $$nairaUnicode : '');
         $right = str_pad($sign.$this->price, $rightCols, ' ', STR_PAD_LEFT);
-<<<<<<< HEAD
-=======
-=======
-        if ($this -> nairaSign) {
-            $leftCols = $leftCols / 2 - $rightCols / 2;
-        }
-        $left = str_pad($this -> name, $leftCols) ;
-
-        $sign = ($this -> nairaSign ? '# ' : '');
-        $right = str_pad($sign . $this -> price, $rightCols, ' ', STR_PAD_LEFT);
->>>>>>> 060766fe05b38dadf2897b881fab97884399e5e3
->>>>>>> 0da628fe696508bd39c21c22e2116dbf7925a3e3
         return "$left$right\n";
     }
 }
